02e2f31fba5754c45ee93c9d5f4cb53b89fa7452
Programista Jeden <programista_jeden@java.pl>
2023-03-20 19:25:47 +0100

    Person 1

--- Person.java
+++ Person.java
@@ -60,4 +60,8 @@ public class Person {
         result = 31 * result + (getAge() != null ? getAge().hashCode() : 0);
         return result;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
