3a45bbc94296ec7e0df74ba2f65208cdba8d626f
Programista Jeden <programista_jeden@java.pl>
2023-03-20 19:25:57 +0100

    Person 2

--- Person.java
+++ Person.java
@@ -63,5 +63,9 @@ public class Person {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
