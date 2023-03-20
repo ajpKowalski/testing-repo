d6d08df555992739245d83b3711a34264472c1aa
Programista Jeden <programista_jeden@java.pl>
2023-03-20 18:38:53 +0100

    Add Car.java

---
+++ Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    private String model;
+    private String brand;
+
+    public Car(String model, String brand) {
+        this.model = model;
+        this.brand = brand;
+    }
+}
