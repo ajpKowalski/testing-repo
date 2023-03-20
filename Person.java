25bc7102da9e885800ec6d98b427670048cd580c
Programista Dwa <programista_dwa@java.pl>
2023-03-20 19:35:17 +0100

    Person 20

--- Person.java
+++ Person.java
@@ -66,5 +66,7 @@ public class Person {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
